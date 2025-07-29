98ef62e88b0db9a1c0c6cf7c608fba958b55ed3b
motta <alvaromotta.dev@gmail.com>
2025-07-29 19:50:34 -0300

    remove unnecessary comments from Contador.java

--- controle-fluxo/src/Contador.java
+++ controle-fluxo/src/Contador.java
@@ -2,8 +2,6 @@ import exception.ParametrosInvalidosException;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Contador {
     public static void main(String[] args) {
 
