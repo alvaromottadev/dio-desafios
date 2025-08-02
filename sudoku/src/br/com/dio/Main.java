c5a64a067a6f6a09863c31c4722d392fe9bcabe3
motta <alvaromotta.dev@gmail.com>
2025-08-02 14:06:21 -0300

    feat: melhorias e correções

--- sudoku/src/br/com/dio/Main.java
+++ sudoku/src/br/com/dio/Main.java
@@ -30,15 +30,17 @@ public class Main {
                 ));
         var option = -1;
         while (true){
-            System.out.println("Selecione uma das opções a seguir");
-            System.out.println("1 - Iniciar um novo Jogo");
-            System.out.println("2 - Colocar um novo número");
-            System.out.println("3 - Remover um número");
-            System.out.println("4 - Visualizar jogo atual");
-            System.out.println("5 - Verificar status do jogo");
-            System.out.println("6 - limpar jogo");
-            System.out.println("7 - Finalizar jogo");
-            System.out.println("8 - Sair");
+            System.out.println("""
+                    Selecione uma das opções a seguir
+                    1 - Iniciar um novo Jogo
+                    2 - Colocar um novo número
+                    3 - Remover um número
+                    4 - Visualizar jogo atual
+                    5 - Verificar status do jogo
+                    6 - limpar jogo
+                    7 - Finalizar jogo
+                    8 - Sair
+                    """);
 
             option = scanner.nextInt();
 
@@ -171,9 +173,9 @@ public class Main {
             showCurrentGame();
             board = null;
         } else if (board.hasErrors()) {
-            System.out.println("Seu jogo conté, erros, verifique seu board e ajuste-o");
+            System.out.println("Seu jogo contém erros, verifique seu board e ajuste-o");
         } else {
-            System.out.println("Você ainda precisa preenhcer algum espaço");
+            System.out.println("Você ainda precisa preencher algum espaço");
         }
     }
 
